d5f4065a0e0dede57a6c77384daba2d7b85e0fa8
Georgij Zacharov <gosha.za@gmail.com>
2022-03-26 00:33:00 +0100

    Bump private-nixpkgs

--- scrobbler.nix
+++ scrobbler.nix
@@ -2,7 +2,7 @@
 let
   # customPkgs = import ../private-nix { }; # For local development
   customPkgs = import (fetchTarball
-    "https://github.com/Gosha/private-nixpkgs/archive/f9787add2eee34344904238c426874725654363d.tar.gz")
+    "https://github.com/Gosha/private-nixpkgs/archive/5f50e5bc139d6c44e9c9c9012c5e2f8ac3863e5b.tar.gz")
     { };
   pythonPackages = pkgs.python3Packages;
   pyanidb = customPkgs.pyanidb;
