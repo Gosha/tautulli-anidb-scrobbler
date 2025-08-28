d4891b749428265d2597f850763dfbd913e70428
Georgij Zacharov <gosha.za@gmail.com>
2025-08-28 18:44:59 +0200

    Update pyanidb

--- scrobbler.nix
+++ scrobbler.nix
@@ -2,7 +2,7 @@
 let
   # customPkgs = import ../private-nix { }; # For local development
   customPkgs = import (fetchTarball
-    "https://github.com/Gosha/private-nixpkgs/archive/5f50e5bc139d6c44e9c9c9012c5e2f8ac3863e5b.tar.gz")
+    "https://github.com/Gosha/private-nixpkgs/archive/b5ccf6eab6d42fc7b65f0d6e1699c39551aaded0.tar.gz")
     { };
   pythonPackages = pkgs.python3Packages;
   pyanidb = customPkgs.pyanidb;
